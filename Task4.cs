2e750ae48157872a39b6e5ea3961c2bd117b01f4
mikbadalyan <mikayel.b.a@gmail.com>
2024-02-02 20:35:02 +0400

    Task 4 is solved.

--- Task4.cs
+++ Task4.cs
@@ -3,57 +3,95 @@
 //Շախմատի տախտակի ստեղծում։
 int rows = 8;
 int columns = 8;
-
+int quant = 0;
 int[,] chess_board = new int[rows, columns];
- 
-for (int i = 0; i < rows; i++)
+
+Console.WriteLine("Please wait up to 5 seconds to see the results...");
+Console.WriteLine("Loading...");
+
+//
+
+while (quant < 60)
 {
-    for (int j = 0; j < columns; j++)
+    quant = 0;   
+    for (int i = 0; i < rows; i++)
     {
-        chess_board[i, j] = 0;
+        for (int j = 0; j < columns; j++)
+        {
+            chess_board[i, j] = 0;
+        }
+    } 
+    //Ստեղծում ենք rand անունով զանգված, որի զույգ էլեմենտների վրա կպահենք տողերի ինդեքսը, իսկ կենտ էլեմենտների վրա սյուների ինդեքսը։
+    int[] rand = new int[16];
+    int num = 0;
+    //
+    Knight knight1 = new Knight();
+    //Ստեղծում ենք knight1 անունով օբյեկտ, որը կունենա նախնական դիրք շախմատի տախտակի վրա (5,6)։
+    knight1.knight_row = 5;
+    knight1.knight_column = 6;
+    knight1.knight_strokes(chess_board, knight1.knight_row, knight1.knight_column, rand, num);
+    knight1.knight_strokes_cleaner(chess_board, knight1.knight_row, knight1.knight_column);
+    
+    quant++;
+
+
+
+    //
+
+    // Քայլ է կատարում հաջորդ հավանական վայրերից մեկում, պատահականության սկզբունքով։
+    Random random = new Random();
+
+    for (int i = 0; i < 20000; i++)
+    {
+        int m = random.Next(0,15);
+        //Console.WriteLine(m);
+        if (m % 2 == 0 && (rand[m] != -1 || rand[m+1] != -1))
+        {
+            Knight knight2 = new Knight();
+            knight2.knight_row = rand[m];
+            knight2.knight_column = rand[m + 1];
+            knight2.knight_strokes_cleaner(chess_board, knight2.knight_row, knight2.knight_column);
+            num = 0;
+            for (int j = 0; j < 16; j++)
+            {
+                rand[j] = -1;
+            }
+            //knight2.knight_strokes_cleaner(chess_board, knight2.knight_row, knight2.knight_column);
+            knight2.knight_strokes(chess_board, knight2.knight_row, knight2.knight_column, rand, num);
+            knight2.knight_strokes_cleaner(chess_board, knight2.knight_row, knight2.knight_column);
+           
+            quant++;
+
+
+        }else if (m % 2 != 0 && (rand[m-1] != -1 || rand[m] != -1))
+        {
+            Knight knight2 = new Knight();
+            knight2.knight_row = rand[m - 1];
+            knight2.knight_column = rand[m];
+            knight2.knight_strokes_cleaner(chess_board, knight2.knight_row, knight2.knight_column);
+            num = 0;
+            for (int j = 0; j < 16; j++)
+            {
+                rand[j] = -1;
+            }
+            
+            knight2.knight_strokes(chess_board, knight2.knight_row, knight2.knight_column, rand, num);
+            knight2.knight_strokes_cleaner(chess_board, knight2.knight_row, knight2.knight_column);
+           
+            quant++;
+        }
+        
     }
-} 
-//
 
-//Ստեղծում ենք rand անունով զանգված, որի զույգ էլեմենտների վրա կպահենք տողերի ինդեքսը, իսկ կենտ էլեմենտների վրա սյուների ինդեքսը։
-int[] rand = new int[15];
-int num = 0;
-//
-
-//Ստեղծում ենք knight1 անունով օբյեկտ, որը կունենա նախնական դիրք շախմատի տախտակի վրա (5,6)։
-Knight knight1 = new Knight();
-knight1.knight_row = 5;
-knight1.knight_column = 6;
-knight1.knight_strokes(chess_board, knight1.knight_row, knight1.knight_column, rand, num);
-knight1.visual(rows, columns, chess_board);
-//
-
-// Քայլ է կատարում հաջորդ հավանական վայրերից մեկում, պատահականության սկզբունքով։
-Random random = new Random();
-int m = random.Next(0,15);
-
-if (m % 2 == 0)
-{
-    Knight knight2 = new Knight();
-    knight2.knight_row = rand[m];
-    knight2.knight_column = rand[m + 1];
-    knight2.knight_strokes_cleaner(chess_board, knight1.knight_row, knight1.knight_column);
-    knight2.knight_strokes(chess_board, knight2.knight_row, knight2.knight_column, rand, num);
-    knight2.visual(rows, columns, chess_board);
     
-}else if (m % 2 != 0)
-{
-    Knight knight2 = new Knight();
-    knight2.knight_row = rand[m - 1];
-    knight2.knight_column = rand[m];
-    knight2.knight_strokes_cleaner(chess_board, knight1.knight_row, knight1.knight_column);
-    knight2.knight_strokes(chess_board, knight2.knight_row, knight2.knight_column, rand, num);
-    knight2.visual(rows, columns, chess_board);
+    //
     
+    //Ստեղծում ենք Knight անունով class-ը, որը իր մեջ կներառի տվյալներ շախմատի ձիու մասին, նրա հնարավոր քայլերի ինչպես նաև վիզուալ պատկերը իր մեջ։ Վերջում նոր քայլ անելից կարող ենք ջնջել նախորդ դիրքերի 1֊երը։
+
 }
-//
-
-//Ստեղծում ենք Knight անունով class-ը, որը իր մեջ կներառի տվյալներ շախմատի ձիու մասին, նրա հնարավոր քայլերի ինչպես նաև վիզուալ պատկերը իր մեջ։ Վերջում նոր քայլ անելից կարող ենք ջնջել նախորդ դիրքերի 1֊երը։
+Knight knight3 = new Knight();
+knight3.visual(rows, columns, chess_board);
+Console.WriteLine(quant);
 class Knight
 {
     public int knight_row;
@@ -77,28 +115,28 @@ class Knight
         for (int i = 1; i < 3; i++)
         {
             int j = 3 - i;
-            if (knight_row + i <= 7 && knight_column + j <= 7)
+            if (knight_row + i <= 7 && knight_column + j <= 7 && chess_board[knight_row + i, knight_column + j] != 9)
             {
                 chess_board[knight_row + i, knight_column + j] = 1;
                 rand[num] = knight_row + i;
                 rand[num + 1] = knight_column + j;
                 num = num + 2;
             }
-            if (knight_row + i <= 7 && knight_column - j >= 0)
+            if (knight_row + i <= 7 && knight_column - j >= 0 && chess_board[knight_row + i, knight_column - j] != 9)
             {
                 chess_board[knight_row + i, knight_column - j] = 1;
                 rand[num] = knight_row + i;
                 rand[num + 1] = knight_column - j;
                 num = num + 2;    
             }
-            if (knight_row - i >= 0 && knight_column - j >= 0)
+            if (knight_row - i >= 0 && knight_column - j >= 0 && chess_board[knight_row - i, knight_column - j] != 9)
             {
                 chess_board[knight_row - i, knight_column - j] = 1;
                 rand[num] = knight_row - i;
                 rand[num + 1] = knight_column - j;
                 num = num + 2;    
             }
-            if (knight_row - i >= 0 && knight_column + j <= 7)
+            if (knight_row - i >= 0 && knight_column + j <= 7 && chess_board[knight_row - i, knight_column + j] != 9)
             {
                 chess_board[knight_row - i, knight_column + j] = 1;
                 rand[num] = knight_row - i;
@@ -109,7 +147,8 @@ class Knight
         }
         chess_board[knight_row, knight_column] = 9;
     }
-    
+
+    public int quantity = 0;
     /// <summary>
     /// Վիզուալ շախմատի տախտակի պատկեր
     /// </summary>
@@ -122,11 +161,21 @@ class Knight
         {
             for (int j = 0; j < columns; j++)
             {
-                Console.Write(chess_board[i, j] + " ");
+                if (chess_board[i, j] == 9)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(chess_board[i, j] + " ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    quantity++;
+                }
+                else
+                {
+                    Console.Write(chess_board[i, j] + " ");
+                }
             }
             Console.WriteLine();
         }
-        Console.WriteLine();
+        Console.WriteLine(quantity);
         Console.WriteLine();
         Console.WriteLine();
     }
@@ -143,22 +192,22 @@ class Knight
         for (int i = 1; i < 3; i++)
         {
             int j = 3 - i;
-            if (knight_row + i <= 7 && knight_column + j <= 7)
+            if (knight_row + i <= 7 && knight_column + j <= 7 && chess_board[knight_row + i, knight_column + j] != 9)
             {
                 chess_board[knight_row + i, knight_column + j] = 0;
                 
             }
-            if (knight_row + i <= 7 && knight_column - j >= 0)
+            if (knight_row + i <= 7 && knight_column - j >= 0 && chess_board[knight_row + i, knight_column - j] != 9)
             {
                 chess_board[knight_row + i, knight_column - j] = 0;
                     
             }
-            if (knight_row - i >= 0 && knight_column - j >= 0)
+            if (knight_row - i >= 0 && knight_column - j >= 0 && chess_board[knight_row - i, knight_column - j] != 9)
             {
                 chess_board[knight_row - i, knight_column - j] = 0;
                 
             }
-            if (knight_row - i >= 0 && knight_column + j <= 7)
+            if (knight_row - i >= 0 && knight_column + j <= 7 && chess_board[knight_row - i, knight_column + j] != 9)
             {
                 chess_board[knight_row - i, knight_column + j] = 0;
                  
@@ -167,5 +216,4 @@ class Knight
         }
     }
 }
-
 
